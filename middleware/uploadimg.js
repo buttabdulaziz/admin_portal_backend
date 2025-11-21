1cb6fe97a0a1dc005ce0e99a0cb937fc35d0828c
Abdul Aziz <abdulazizbutt245@gmail.com>
2025-11-21 10:40:47 +0500

    img upload

--- middleware/uploadimg.js
+++ middleware/uploadimg.js
@@ -1,1 +1,27 @@
 import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination:(req, file, cb)=> {
+        cb(null, "upload/products/");
+    },
+    filename: (req, file, cb)=> {
+        cb(null, Date.now() + "-" + path.extname(file.originalname));
+
+    }
+})
+
+const extension = (req, file, cb) => {
+    const filetypes = ['image/jpeg', 'image/jpg', 'image/png'];
+    if (filetypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(new Error('Only .jpeg, .jpg and .png format allowed!') , false);
+    }
+}
+
+export const upload = multer({
+    storage: storage,
+    fileFilter: extension,
+    limits: { fileSize: 1024 * 1024 * 5 } // 5MB limit
+});
